c8dea403090e2f1e264f87c22f290ec8f078b60f
Okojay <mavis4me1@gmail.com>
2024-12-15 19:24:09 +0100

    Add files via upload

---
+++ Project/script.js
@@ -0,0 +1,28 @@
+// function myFunction(){
+//   let x = document.getElementById('heading');
+//   x.style.background = "blue";
+//   x.style.color = "white";
+//   x.style.width = "20%"
+//   x.style.height = "50px"
+//   x.style.fontSize = "20px"
+//   x.style.fontFamily = "Sans-Serif"
+// }
+// function mySecondFunction(){
+//   let x = document.getElementById('heading');
+//   x.style.background = "none";
+//   x.style.color = "black";
+//   x.style.width = "10%"
+//   x.style.height = "10px"
+//   x.style.fontSize = "10px"
+//   x.style.fontFamily = "Serif"
+// }
+// var imgObj = null;
+// function init(){
+//   imgObj = document.getElementById('myImage');
+//   imgObj.style.position = 'relative';
+//   imgObj.style.left = '0px';
+// }
+// function myAnimation(){
+//   imgObj.style.left = 120 + 'px';
+// }
+// window.onload = init;
